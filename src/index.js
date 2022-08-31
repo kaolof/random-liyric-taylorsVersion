800808e044cb0fe0b56624187994b8239b859e59
Kao Lo <kaochilanlo@gmail.com>
2022-08-31 13:17:47 -0400

    inicial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+albums=["Taylor Swift","Fearless","Speak Now","Red","1989","Speak Now","Reputation","Lover","Folklore","Evermore"];
+const url="https://taylorswiftapi.herokuapp.com/get?album=";
+const cardNode=document.querySelector('.card');
+const clickMe=document.querySelector('.clickme');
+
+//creando componentes 
+const quote=document.createElement('h3');
+const nameSong=document.createElement('p');
+const nameAlbum=document.createElement('p');
+
+clickMe.addEventListener('click',()=>{
+    //agregando estilo de card
+    
+    const random = Math.floor(Math.random() * 10);
+    const urlAlbum=`${url}${albums[random]}`;
+    const info=document.createElement("div");
+    window
+    .fetch(urlAlbum)
+    .then(respuesta=> respuesta.json())
+    .then((data)=>{
+        cardNode.classList.add('wrapper', 'block');
+        quote.textContent=`"${data.quote}"`;
+        nameSong.textContent=`- ${data.song}`;
+        nameAlbum.textContent=`💿 ${data.album}`;
+        
+
+        info.append(quote, nameSong, nameAlbum);
+        cardNode.appendChild(info);
+
+    });
+    
+});
+
+
+   
