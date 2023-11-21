a6deb565a51c2153a12c60d735b0d861658c92d4
Kao <kaochilanlo@egmail.com>
2023-11-21 09:53:58 -0400

    colors eras modifided

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 var albums = [
-    { album: "Taylor Swift", color: "#bad3b6" },
-    { album: "Fearless", color: "#f4cd8c" },
-    { album: "Speak Now", color: "#d2b7d6" },
-    { album: "Red", color: "#823142" },
-    { album: "1989", color: "#bee9f9" },
-    { album: "Reputation", color: "#888083" },
-    { album: "Lover", color: "#888083" },
+    { album: "Taylor Swift", color: "#bdd6b9" },
+    { album: "Fearless", color: "#f8cd93" },
+    { album: "Speak Now", color: "#ceb3d2" },
+    { album: "Red", color: "#8a3a4b" },
+    { album: "1989", color: "#bee9fc" },
+    { album: "Reputation", color: "#857e80" },
+    { album: "Lover", color: "#f8b6d2" },
     { album: "Folklore", color: "#d1d0cb" },
-    { album: "Evermore", color: "#cbb89a" }
+    { album: "Evermore", color: "#c9b59c" }
 ];
 const url="https://taylorswiftapi.onrender.com/get?album=";
 const cardNode=document.querySelector('.card');
@@ -21,16 +21,18 @@ const nameSong=document.createElement('p');
 const nameAlbum=document.createElement('p');
 
 clickMe.addEventListener('click',()=>{
+    console.log("click")
     //agregando estilo de card
     const random = Math.floor(Math.random() * 10);
     const urlAlbum=`${url}${albums[random].album}`;
+    console.log(urlAlbum);
     const info=document.createElement("div");
-    const colorEra= albums[random].color
+    const colorEra= albums[random].color;
     window
     .fetch(urlAlbum)
     .then(respuesta=> respuesta.json())
     .then((data)=>{
-        //body.style.backgroundColor=colorEra //cambiar el color de fondo dependiendo de la era
+        body.style.backgroundColor=colorEra //cambiar el color de fondo dependiendo de la era
         cardNode.classList.add('wrapper', 'block');
         quote.textContent=`"${data.quote}"`;
         nameSong.textContent=`- ${data.song}`;
